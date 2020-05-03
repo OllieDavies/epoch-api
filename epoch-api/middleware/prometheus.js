d12352d9bb83543f6a6af92756584f483b76dca9
Ollie Davies <ollie.davies@magairports.com>
2020-05-03 12:17:39 +0100

    Added Prometheus middleware

---
+++ epoch-api/middleware/prometheus.js
@@ -0,0 +1,7 @@
+const prometheusMiddleware = require('express-prometheus-middleware');
+
+module.exports = prometheusMiddleware({
+  metricsPath: '/metrics',
+  collectDefaultMetrics: true,
+  requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+})
